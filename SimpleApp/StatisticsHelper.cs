c06e6bbacbc69341f95aaeb36a0879ffe4987395
Danylo Burukhin <s30030@pjwstk.edu.pl>
2026-03-18 21:19:27 +0100

    Add Calculate method

--- SimpleApp/StatisticsHelper.cs
+++ SimpleApp/StatisticsHelper.cs
@@ -6,6 +6,22 @@ namespace SimpleApp
         {
             return age >= 18;
         }
+
+        public static int CalculateMax(int[] values)
+{
+    if (values == null || values.Length == 0)
+        return 0;
+
+    int max = values[0];
+
+    foreach (var v in values)
+    {
+        if (v > max)
+            max = v;
+    }
+
+    return max;
+}
     
 
     public static double CalculateAverage(int[] values)
