b818dcf0b2fb2d040bc3e6ae48be06c3833da022
Danylo Burukhin <s30030@pjwstk.edu.pl>
2026-03-18 21:09:36 +0100

    feat: add CalculateAverage method to StatisticsHelpeer

--- SimpleApp/StatisticsHelper.cs
+++ SimpleApp/StatisticsHelper.cs
@@ -6,5 +6,18 @@ namespace SimpleApp
         {
             return age >= 18;
         }
+    
+
+    public static double CalculateAverage(int[] values)
+{
+    if (values == null || values.Length == 0)
+        return 0;
+
+    double sum = 0;
+    foreach (var v in values)
+        sum += v;
+
+    return sum / values.Length;
+        }
     }
 }
